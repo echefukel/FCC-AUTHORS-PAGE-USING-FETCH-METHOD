c9237c86c89902ea9cf3db300520e512a3e4e7ac
Kelechi Obinna Echefu <80517102+echefukel@users.noreply.github.com>
2024-07-10 10:36:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const authorContainer = document.getElementById('author-container');
+const loadMoreBtn = document.getElementById('load-more-btn');
+
+let startingIndex = 0;
+let endingIndex = 8;
+let authorDataArr = [];
+
+
+// make a get request to the FCC url
+fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json').then((res)=> res.json()).then((data)=>{
+
+    authorDataArr = data;
+     displayAuthors(authorDataArr.slice(startingIndex,endingIndex))
+}).catch((err)=>{
+    authorContainer.innerHTML = '<p class="error-msg">There was an error loading the authors</p>';
+});
+
+// create function to fetch more authors
+
+const fetchMoreAuthors = ()=>{
+    startingIndex += 8;
+    endingIndex += 8;
+    displayAuthors(authorDataArr.slice(startingIndex,endingIndex))
+     if(authorDataArr.length <= endingIndex){
+        loadMoreBtn.disabled = true;
+        loadMoreBtn.textContent = 'No more data to load';
+        loadMoreBtn.style.cursor = 'not-allowed'
+     }
+
+}
+
+
+
+
+// create a function to populate UI
+const displayAuthors = (authors)=>{
+   authors.forEach(({author,image,url,bio},index)=>{
+       authorContainer.innerHTML += `<div id =${index} class="user-card">
+       <h2 class="author-name">${author}</h2>
+       <img class="user-img" src="${image}" alt="${author} avatar" />
+       <div class='purple-divider'></div>
+        <p class="bio">${bio.length > 50? bio.slice(0,50) + '...': bio}</p>
+        <a class="author-link" href="${url}" target="_blank">${author}'s author page</a>
+
+       </div>`
+   });
+};
+
+
+
+// add an event listener to load more buttons;
+loadMoreBtn.addEventListener('click',fetchMoreAuthors);
